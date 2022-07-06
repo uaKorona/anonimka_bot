8164f40ca752ddc04f5108972ee4405e814001f0
Roman Kovalskyi <rkovalskiy@deloitte.com>
2022-07-06 22:49:40 +0300

    web-hook

--- src/bot.ts
+++ src/bot.ts
@@ -3,8 +3,19 @@ import { Update } from 'typegram';
 import { MESSAGES } from "./messeges";
 import { MessageTypes } from "./message-types.enum";
 import { botHelper } from "./bot-helper";
+import LaunchOptions = Telegraf.LaunchOptions;
 
-const { ANONIMKA_BOT_TOKEN, ANONIMKA_CHAT_ID } = process.env;
+
+const { ANONIMKA_BOT_TOKEN, ANONIMKA_CHAT_ID, HEROKU_APP_NAME, PORT } = process.env;
+// webhook settings
+const WEBHOOK_HOST = `https://${HEROKU_APP_NAME}.herokuapp.com`;
+// https://anonimka-avtorika-bot.herokuapp.com/
+const WEBHOOK_PATH = `webhook/${ANONIMKA_BOT_TOKEN}`;
+const WEBHOOK_URL = `${WEBHOOK_HOST}/${WEBHOOK_PATH}`;
+
+// webserver settings
+const WEBAPP_HOST = '0.0.0.0'
+const WEBAPP_PORT = PORT || 8000;
 
 export const bot: Telegraf<Context<Update>> = new Telegraf( ANONIMKA_BOT_TOKEN as string );
 
@@ -28,3 +39,12 @@ bot.on( [MessageTypes.photo, MessageTypes.video, MessageTypes.document], ( ctx )
 bot.use(( ctx ) => {
     ctx.replyWithHTML( MESSAGES.unSupportType() );
 });
+
+const config: LaunchOptions = {
+    webhook: {
+        domain: WEBHOOK_HOST,
+        port: 4000
+    }
+}
+
+bot.launch(config)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { bot } from "./bot";
 
-bot.launch();
-
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
